8594b511d27adb4436c0307f5efb68b4bee51831
AccMaryamSAIT <maryam.bunama@edu.sait.ca>
2023-05-15 11:28:50 -0600

    edited notes

--- assignmentnotes.py
+++ assignmentnotes.py
@@ -1,21 +1,15 @@
 '''
 Use command:
-
 python python_file directory record c
 ----------------------------------------------------------------------------------------------
 python name_of_pythonfile Directory_we_want_to_check Record-file-to-check-against c(c creates0)
 use a hash functions
 
+What the program does:
 tells us if file is the same, changed, or deleted, or a new file was created
-
 checks against a directory
 
-libraries add functionality 
-sys and os are python libraries
-libraries add weight to a program
+code has error checking 
+code uses hashing 
 
-documentations
-
-title page
-business process
 '''
