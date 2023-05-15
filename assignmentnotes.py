9cdd047623384849ef0d10cc805196dd25d8ef7d
AccMaryamSAIT <maryam.bunama@edu.sait.ca>
2023-05-15 11:22:08 -0600

    addd files

---
+++ assignmentnotes.py
@@ -0,0 +1,21 @@
+'''
+Use command:
+
+python python_file directory record c
+----------------------------------------------------------------------------------------------
+python name_of_pythonfile Directory_we_want_to_check Record-file-to-check-against c(c creates0)
+use a hash functions
+
+tells us if file is the same, changed, or deleted, or a new file was created
+
+checks against a directory
+
+libraries add functionality 
+sys and os are python libraries
+libraries add weight to a program
+
+documentations
+
+title page
+business process
+'''
