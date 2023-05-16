671076ba5a77654a7354687a3bafd75f13e1cbd4
AccMaryamSAIT <maryam.bunama@edu.sait.ca>
2023-05-16 10:45:35 -0600

    added some pseudocode

--- Tripwire.py
+++ Tripwire.py
@@ -1,8 +1,12 @@
 '''
 PSEUDOCODE
 
-1. 
-2. 
+1. Read contents of tripwireDir
+    a. check if file exists
+    b. if not, print an error
+2. Create a hash and file name pair of files in tripwireDir
+    a. Create tripwireRecord
+    b. Write information to tripwireRecord
 3. 
 4. 
 
